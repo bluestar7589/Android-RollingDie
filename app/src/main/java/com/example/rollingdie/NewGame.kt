9c0419c2c1e6d64d4b042f04d5e5db8bb1cd15cc
bluestar7589 <bluestar7589@yahoo.com>
2024-11-13 07:38:01 -0800

    Re-design the layout

--- app/src/main/java/com/example/rollingdie/NewGame.kt
+++ app/src/main/java/com/example/rollingdie/NewGame.kt
@@ -9,7 +9,14 @@ import androidx.navigation.Navigation
 import com.example.rollingdie.databinding.GameNewBinding
 
 class NewGame : Fragment() {
+    //This holds the binding object for the fragment's layout.
+    // It is nullable to allow setting it to null when the view is destroyed.
     private var _binding: GameNewBinding? = null
+
+    //A private read-only property that returns the non-nullable _binding variable.
+    // The !! operator is used to assert that _binding is not null when accessed.
+    // This property provides a safe way to access the binding object
+    // without having to check for nullability every time.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -20,6 +27,8 @@ class NewGame : Fragment() {
         return binding.root
     }
 
+    // Function is called after the fragment's view has been created
+    // and when the button was clicked, it navigates to the RollingDie fragment
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -29,6 +38,8 @@ class NewGame : Fragment() {
         }
     }
 
+    // Function is called when the fragment's view is being destroyed
+    // which helps to avoid memory leaks
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
